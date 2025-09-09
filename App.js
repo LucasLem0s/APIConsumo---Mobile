e3293102b1ab07c07af7aff8740ec5a410bb05af
LucasLem0s <lucaslemoss@live.com>
2025-09-09 20:52:08 -0300

    API adicionada

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React, {useState,useEffect} from "react";  
-import {Text, View, Flatlist} from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, FlatList, Image } from "react-native";
 import { estilo } from "./components/estiloApp";
 
 const request = async (callback) => {
-  const response  = await fetch('https://swapi.dev/api/people/');
+  const response = await fetch("https://randomfox.ca/floof/");
   const parsed = await response.json();
-  callback(parsed.results);
-}
+  callback([parsed]); 
+};
 
 export default function App() {
   const [registros, setRegistros] = useState([]);
@@ -18,18 +18,22 @@ export default function App() {
   return (
     <View style={estilo.container}>
       <View>
-        <Text style={estilo.superior}>Star Wars</Text>
-      </View>     
+        <Text style={estilo.superior}>Random FOX</Text>
+      </View>
 
-      <Flatlist
+      <FlatList
         data={registros}
-        keyExtractor={(item) => item.name.toString()}
-        renderItem={({item}) => 
-        <Text style={estilo.item}>
-        <Text> Nome: {item.id}{'\n'}</Text>
-        <Text> Peso: {item.nome}{'\n'}</Text>
-        </Text>
-        }
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={{ marginBottom: 20 }}>
+            <Text style={estilo.item}>Imagem da Raposa:</Text>
+            <Image
+              source={{ uri: item.image }}
+              style={{ width: 250, height: 250, resizeMode: "contain" }}
+            />
+            <Text style={estilo.item}>Link: {item.link}</Text>
+          </View>
+        )}
       />
     </View>
   );
